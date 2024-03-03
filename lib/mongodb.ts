ceef05d1064fe113206d6ef8f6e881af3638302b
aayushb03 <abbharadwaj@wisc.edu>
2024-03-03 17:24:43 -0600

    Project done

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -6,19 +6,7 @@ const uri = 'mongodb+srv://dane:test@testcluster.kyu97aj.mongodb.net/?retryWrite
 let client;
 let clientPromise: Promise<MongoClient>;
 
-// if (process.env.NODE_ENV === 'development') {
-//   let globalWithMongo = global as typeof globalThis & {
-//     _mongoClientPromise?: Promise<MongoClient>;
-//   }
-//
-//   if (!globalWithMongo._mongoClientPromise) {
-//     client = new MongoClient(uri);
-//     globalWithMongo._mongoClientPromise = client.connect();
-//   }
-//   clientPromise = globalWithMongo._mongoClientPromise;
-// } else {
-  client = new MongoClient(uri);
-  clientPromise = client.connect();
-// }
+client = new MongoClient(uri);
+clientPromise = client.connect();
 
 export default clientPromise;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import {useEffect, useState} from "react";
 import { Restaurant_Type } from "@/lib/types";
 import {getAllRestaurants} from "@/lib/Service";
+import { Inter } from "next/font/google";
+import Restaurant from "@/components/restaurant";
 
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [restaurants, setRestaurants] = useState<Restaurant_Type[]>([]);
@@ -13,14 +16,14 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
-      {restaurants && restaurants.map((restaurant) => (
-        <div key={restaurant.id}>
-          <h2>{restaurant.name}</h2>
-          <p>{restaurant.cuisine}</p>
-          <p>{restaurant.borough}</p>
-        </div>
-      ))}
+    <div className={"flex flex-col justify-center items-center w-full"}>
+      <h1 className={"text-2xl font-bold text-red-400 py-4"}>Restaurants in New York</h1>
+      <div className={"flex flex-col gap-2"}>
+        {restaurants && restaurants.map((restaurant, i) => (
+          <Restaurant restaurant={restaurant} key={i} />
+        ))}
+      </div>
     </div>
+
   );
 }
